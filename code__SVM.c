124bd33af3802b2998992d6484c2c34b4d5510d3
DWildShace <dwildshace1000@gmail.com>
2025-02-25 16:34:02 +0700

    chay ok ma can cai thien them do chinh xãc

--- code__SVM.c
+++ code__SVM.c
@@ -2,70 +2,67 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define N 100  // S? lu?ng di?m d? li?u
+#define DIM 2  // S? chi?u c?a d? li?u
 #define MAX_ITER 1000
 #define LEARNING_RATE 0.01
 
-typedef struct {
-    double *weights;
-    double bias;
-} SVM;
+double weights[DIM];
+double bias;
 
-double dot_product(double *a, double *b, int size) {
+void initialize() {
+    for (int i = 0; i < DIM; i++) {
+        weights[i] = (double)rand() / RAND_MAX;
+    }
+    bias = (double)rand() / RAND_MAX;
+}
+
+double dot_product(double *v1, double *v2) {
     double result = 0.0;
-    for (int i = 0; i < size; i++) {
-        result += a[i] * b[i];
+    for (int i = 0; i < DIM; i++) {
+        result += v1[i] * v2[i];
     }
     return result;
 }
 
-void train(SVM *svm, double **X, double *y, int n_samples, int n_features) {
-    svm->weights = (double *)malloc(n_features * sizeof(double));
-    for (int i = 0; i < n_features; i++) {
-        svm->weights[i] = 0.0;
-    }
-    svm->bias = 0.0;
+int predict(double *x) {
+    return dot_product(weights, x) + bias > 0 ? 1 : -1;
+}
 
+void train(double X[N][DIM], int y[N]) {
     for (int iter = 0; iter < MAX_ITER; iter++) {
-        for (int i = 0; i < n_samples; i++) {
-            double condition = y[i] * (dot_product(svm->weights, X[i], n_features) + svm->bias);
-            if (condition < 1) {
-                for (int j = 0; j < n_features; j++) {
-                    svm->weights[j] += LEARNING_RATE * (y[i] * X[i][j] - 2 * (1.0 / MAX_ITER) * svm->weights[j]);
+        for (int i = 0; i < N; i++) {
+            if (y[i] * (dot_product(weights, X[i]) + bias) < 1) {
+                for (int j = 0; j < DIM; j++) {
+                    weights[j] += LEARNING_RATE * (y[i] * X[i][j] - 2 * 0.01 * weights[j]);
                 }
-                svm->bias += LEARNING_RATE * y[i];
+                bias += LEARNING_RATE * y[i];
             } else {
-                for (int j = 0; j < n_features; j++) {
-                    svm->weights[j] += LEARNING_RATE * (-2 * (1.0 / MAX_ITER) * svm->weights[j]);
+                for (int j = 0; j < DIM; j++) {
+                    weights[j] -= LEARNING_RATE * 2 * 0.01 * weights[j];
                 }
             }
         }
     }
 }
 
-double predict(SVM *svm, double *x, int n_features) {
-    double result = dot_product(svm->weights, x, n_features) + svm->bias;
-    return result;
-}
-
 int main() {
-    // Example usage
-    int n_samples = 4;
-    int n_features = 2;
-    double *X[] = {
-        (double[]){2, 3},
-        (double[]){1, 1},
-        (double[]){2, 1},
-        (double[]){3, 2}
-    };
-    double y[] = {1, -1, -1, 1};
+    double X[N][DIM];
+    int y[N];
 
-    SVM svm;
-    train(&svm, X, y, n_samples, n_features);
+    // Kh?i t?o d? li?u và nhãn
+    for (int i = 0; i < N; i++) {
+        X[i][0] = (double)rand() / RAND_MAX;
+        X[i][1] = (double)rand() / RAND_MAX;
+        y[i] = (X[i][0] + X[i][1] > 1) ? 1 : -1;
+    }
 
-    double test_sample[] = {2, 2};
-    double prediction = predict(&svm, test_sample, n_features);
-    printf("Prediction: %f\n", prediction);
+    initialize();
+    train(X, y);
 
-    free(svm.weights);
+    // Ki?m tra d? doán
+    double test[DIM] = {0.6, 0.7};
+    printf("Prediction for test data: %d\n", predict(test));
+
     return 0;
 }
